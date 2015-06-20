dc8f6b4fea29194a4cf577631da3e794272a8013
Darius Conger <dconger@htva.net>
2015-06-20 16:55:55 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file will create a special matrix object that can cache its inverse
+
 
 ## Write a short comment describing this function
 
